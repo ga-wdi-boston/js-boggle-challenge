2c1f1c3d484d0dc4b42db5ce625bd00fdddf24bb
Danny Kirschner <rkirschner1377@gmail.com>
2017-03-21 11:10:01 -0400

    Udating from node-template
    Refresh for cohort lm01. Also did grunt make-standard

--- bin/challenge.js
+++ bin/challenge.js
@@ -1,10 +1,10 @@
-'use strict';
+'use strict'
 
-const boggleVariants = require('../lib/challenge.js');
-const boggle = boggleVariants.boggle;
-const bigBoggle = boggleVariants.bigBoggle;
-const superBigBoggle = boggleVariants.superBigBoggle;
+const boggleVariants = require('../lib/challenge.js')
+const boggle = boggleVariants.boggle
+const bigBoggle = boggleVariants.bigBoggle
+const superBigBoggle = boggleVariants.superBigBoggle
 
-const boggleTray = 'wxxxxoxxxxrxxxxd'.split('');
+const boggleTray = 'wxxxxoxxxxrxxxxd'.split('')
 
-console.log(boggle(boggleTray, [[0, 0], [1, 1], [2, 2], [3, 3]]));
+console.log(boggle(boggleTray, [[0, 0], [1, 1], [2, 2], [3, 3]]))
--- spec/challenge.spec.js
+++ spec/challenge.spec.js
@@ -1,124 +1,119 @@
-'use strict';
+'use strict'
 
-const chai = require('chai');
-const expect = chai.expect;
+const chai = require('chai')
+const expect = chai.expect
 
-const boggleVariants = require('../lib/challenge.js');
+const boggleVariants = require('../lib/challenge.js')
 
 describe('challenge', () => {
-
   describe('Boggle', () => {
-    const boggle = boggleVariants.boggle;
-    const tray = 'dibtloambcgrumps'.split('');
+    const boggle = boggleVariants.boggle
+    const tray = 'dibtloambcgrumps'.split('')
 
-    let dogs = [
+    const dogs = [
       [0, 0],
       [1, 1],
       [2, 2],
-      [3, 3],
-    ];
+      [3, 3]
+    ]
 
     it(`returns 'dogs' for ${dogs.join(';')}`, () => {
-      expect(boggle(tray, dogs)).to.equal('dogs');
-    });
+      expect(boggle(tray, dogs)).to.equal('dogs')
+    })
 
-    let cat = [
+    const cat = [
       [2, 1],
       [1, 2],
-      [0, 3],
-    ];
+      [0, 3]
+    ]
 
     it(`returns 'cat' for ${cat.join(';')}`, () => {
-      expect(boggle(tray, cat)).to.equal('cat');
-    });
+      expect(boggle(tray, cat)).to.equal('cat')
+    })
 
-    let loam = [
+    const loam = [
       [1, 0],
       [1, 1],
       [1, 2],
-      [1, 3],
-    ];
+      [1, 3]
+    ]
 
     it(`returns 'loam' for ${loam.join(';')}`, () => {
-      expect(boggle(tray, loam)).to.equal('loam');
-    });
-
-  });
+      expect(boggle(tray, loam)).to.equal('loam')
+    })
+  })
 
   xdescribe('Big Boggle', () => {
-    const boggle = boggleVariants.bigBoggle;
-    const tray = 'ndehywdibtfloamvbcgrjumps'.split('');
+    const boggle = boggleVariants.bigBoggle
+    const tray = 'ndehywdibtfloamvbcgrjumps'.split('')
 
-    let dogs = [
+    const dogs = [
       [1, 1],
       [2, 2],
       [3, 3],
-      [4, 4],
-    ];
+      [4, 4]
+    ]
 
     it(`returns 'dogs' for ${dogs.join(';')}`, () => {
-      expect(boggle(tray, dogs)).to.equal('dogs');
-    });
+      expect(boggle(tray, dogs)).to.equal('dogs')
+    })
 
-    let cat = [
+    const cat = [
       [3, 2],
       [2, 3],
-      [1, 4],
-    ];
+      [1, 4]
+    ]
 
     it(`returns 'cat' for ${cat.join(';')}`, () => {
-      expect(boggle(tray, cat)).to.equal('cat');
-    });
+      expect(boggle(tray, cat)).to.equal('cat')
+    })
 
-    let loam = [
+    const loam = [
       [2, 1],
       [2, 2],
       [2, 3],
-      [2, 4],
-    ];
+      [2, 4]
+    ]
 
     it(`returns 'loam' for ${loam.join(';')}`, () => {
-      expect(boggle(tray, loam)).to.equal('loam');
-    });
-
-  });
+      expect(boggle(tray, loam)).to.equal('loam')
+    })
+  })
 
   xdescribe('Super Big Boggle', () => {
-    const boggle = boggleVariants.superBigBoggle;
-    const tray = 'ndehykwdibtefloamsvbcgrijumpslpearnt'.split('');
+    const boggle = boggleVariants.superBigBoggle
+    const tray = 'ndehykwdibtefloamsvbcgrijumpslpearnt'.split('')
 
-    let dogs = [
+    const dogs = [
       [1, 1],
       [2, 2],
       [3, 3],
-      [4, 4],
-    ];
+      [4, 4]
+    ]
 
     it(`returns 'dogs' for ${dogs.join(';')}`, () => {
-      expect(boggle(tray, dogs)).to.equal('dogs');
-    });
+      expect(boggle(tray, dogs)).to.equal('dogs')
+    })
 
-    let cat = [
+    const cat = [
       [3, 2],
       [2, 3],
-      [1, 4],
-    ];
+      [1, 4]
+    ]
 
     it(`returns 'cat' for ${cat.join(';')}`, () => {
-      expect(boggle(tray, cat)).to.equal('cat');
-    });
+      expect(boggle(tray, cat)).to.equal('cat')
+    })
 
-    let loam = [
+    const loam = [
       [2, 1],
       [2, 2],
       [2, 3],
-      [2, 4],
-    ];
+      [2, 4]
+    ]
 
     it(`returns 'loam' for ${loam.join(';')}`, () => {
-      expect(boggle(tray, loam)).to.equal('loam');
-    });
-
-  });
-
-});
+      expect(boggle(tray, loam)).to.equal('loam')
+    })
+  })
+})
